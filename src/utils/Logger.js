523e36238985a45cd74aa7f39658f10c46294a11
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2023-02-28 20:53:27 -0300

    import log level from config instead of envs

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,7 +1,8 @@
 const log4js = require('log4js');
+const { LOG_LEVEL } = require('../config');
 
 const logger = log4js.getLogger();
 
-logger.level = process.env.LOG_LEVEL || 'debug';
+logger.level = LOG_LEVEL;
 
 module.exports = logger;
